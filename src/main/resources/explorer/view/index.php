54ec8f443a7117d2311cebb921d03db622a46cfb
purchess <purchess@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-07-21 08:14:03 +0000

    sexy scripts

--- src/main/resources/explorer/view/index.php
+++ src/main/resources/explorer/view/index.php
@@ -2,8 +2,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-<title><?php echo EXPLORER_NAME; ?></title>
+  <title><?php echo EXPLORER_NAME; ?></title>
+  <link href="css/explorer.css" rel="stylesheet" type="text/css" />
+  <script src="js/jquery-1.4.2.min.js" type="text/javascript"></script>
+  <script src="js/script.js" type="text/javascript"></script>
 </head>
 <body>
   <div id="ex__left">
